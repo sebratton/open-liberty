0ef2e7395e7080ca08591abbb6c1aeb53c61263e
Samuel E Bratton <sebratton@users.noreply.github.com>
2020-05-17 17:25:06 -0500

    update coprright year in test-bundles

--- dev/com.ibm.ws.app.manager.wab.installer_fat/test-bundles/test.wab3/src/test/wab3/Servlet3.java
+++ dev/com.ibm.ws.app.manager.wab.installer_fat/test-bundles/test.wab3/src/test/wab3/Servlet3.java
@@ -3,7 +3,7 @@
  *
  * OCO Source Materials
  *
- * WLP Copyright IBM Corp. 2015
+ * WLP Copyright IBM Corp. 2020
  *
  * The source code for this program is not published or otherwise divested 
  * of its trade secrets, irrespective of what has been deposited with the 
